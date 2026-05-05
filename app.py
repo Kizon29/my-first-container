64db1716c9898d59305a9e6079db2086628a755d
kizon29 <guokizon@gmail.com>
2026-05-05 11:36:58 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello Cloud Engineer! My container is running."
+
+if __name__ == '__main__':
+    # This tells the app to listen on all available networks
+    app.run(host='0.0.0.0', port=5000)
